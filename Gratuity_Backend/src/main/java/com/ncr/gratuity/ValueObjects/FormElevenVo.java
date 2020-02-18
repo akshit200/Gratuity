a2398b2075fbdc6b0bc820c1b33792d701c5a786
Ankitag95 <47318101+Ankitag95@users.noreply.github.com>
2020-02-18 16:26:26 +0530

    Value Objects

---
+++ Gratuity_Backend/src/main/java/com/ncr/gratuity/ValueObjects/FormElevenVo.java
@@ -0,0 +1,100 @@
+package com.ncr.gratuity.ValueObjects;
+
+public class FormElevenVo {
+	
+	private String first_name;
+    private String last_name;
+    private String father_name;
+    private String gender;
+    private String marital_status;
+    private String Email_Id;
+    private String Mobile_no;
+    private Boolean Scheme_1952;
+    private Boolean Scheme_1955;
+    private Boolean International_worker;
+    private String IFS_Code;
+    private String Aadhar_no;
+    private String PAN_no;
+    
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getFather_name() {
+		return father_name;
+	}
+	public void setFather_name(String father_name) {
+		this.father_name = father_name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMarital_status() {
+		return marital_status;
+	}
+	public void setMarital_status(String marital_status) {
+		this.marital_status = marital_status;
+	}
+	public String getEmail_Id() {
+		return Email_Id;
+	}
+	public void setEmail_Id(String email_Id) {
+		Email_Id = email_Id;
+	}
+	public String getMobile_no() {
+		return Mobile_no;
+	}
+	public void setMobile_no(String mobile_no) {
+		Mobile_no = mobile_no;
+	}
+	public Boolean getScheme_1952() {
+		return Scheme_1952;
+	}
+	public void setScheme_1952(Boolean scheme_1952) {
+		Scheme_1952 = scheme_1952;
+	}
+	public Boolean getScheme_1955() {
+		return Scheme_1955;
+	}
+	public void setScheme_1955(Boolean scheme_1955) {
+		Scheme_1955 = scheme_1955;
+	}
+	public Boolean getInternational_worker() {
+		return International_worker;
+	}
+	public void setInternational_worker(Boolean international_worker) {
+		International_worker = international_worker;
+	}
+	public String getIFS_Code() {
+		return IFS_Code;
+	}
+	public void setIFS_Code(String iFS_Code) {
+		IFS_Code = iFS_Code;
+	}
+	public String getAadhar_no() {
+		return Aadhar_no;
+	}
+	public void setAadhar_no(String aadhar_no) {
+		Aadhar_no = aadhar_no;
+	}
+	public String getPAN_no() {
+		return PAN_no;
+	}
+	public void setPAN_no(String pAN_no) {
+		PAN_no = pAN_no;
+	}
+    
+    
+
+}
